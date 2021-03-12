5fcf18a6b657dc6382b89d2ce462a0a89ec52db7
sara-gheith <saraannegheith@gmail.com>
2021-03-12 19:12:00 +0100

    Quoting Strings with Single Quotes

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,7 +1,7 @@
 function main() {
     // Only change code below this line
 
-    var myLink = '<a href="http://www.example.com\" target="_blank">Link</a>';
+    var myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
 
     // Only change code above this line
     return myLink;
